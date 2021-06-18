aa96c54903bfa7e2d31edbd1ca8d7fb89287e27d
yappeizhen <66234273+yappeizhen@users.noreply.github.com>
2021-06-19 01:35:29 +0800

    sizing

--- src/views/Home.js
+++ src/views/Home.js
@@ -16,77 +16,31 @@ function Home() {
                 <Link href="#socialImpact" title="Social Impact" />
             </Anchor>
             <div id="profile" />
-            <section className="home-section">
+            <section className="home-intro-section">
                 <Introduction person={personalInfoData} />
                 <div id="work" />
             </section>
             <section className="home-section">
                 <h2 className="section-title">Work Experience</h2>
                 <br />
-                <Row>
-                    <Col lg={12}>
-                        <Thumbnail experience={workExperienceData.aphrodite} />
-                    </Col>
-                    <Col lg={12}>
-                        <Thumbnail experience={workExperienceData.solveware} />
-                    </Col>
-                </Row>
-                <br />
-                <br />
-                <Row>
-                    <Col lg={12}>
-                        <Thumbnail experience={workExperienceData.consulting} />
-                    </Col>
-                    <Col lg={12}>
-                        <Thumbnail experience={workExperienceData.peoplesAssociation} />
-                    </Col>
-                </Row>
-                <div id="projects" />
+                <div className="card-display">
+                    {workExperienceData.map((item) => (<Thumbnail experience={item} />))}
+                </div>
             </section>
             <section className="home-section">
                 <h2 id="projects" className="section-title">Projects</h2>
                 <br />
-                <Row>
-                    <Col lg={12}>
-                        <Thumbnail experience={projectData.mapTheSystem} />
-                    </Col>
-                    <Col lg={12}>
-                        <Thumbnail experience={projectData.perfectMatch} />
-                    </Col>
-                </Row>
-                <br />
-                <br />
-                <Row>
-                    <Col lg={12}>
-                        <Thumbnail experience={projectData.sustainathon} />
-                    </Col>
-                    <Col lg={12}>
-                        <Thumbnail experience={projectData.scdfxibm} />
-                    </Col>
-                </Row>
-                <br />
-                <br />
-                <Row>
-                    <Col lg={12}>
-                        <Thumbnail experience={projectData.fishbowl} />
-                    </Col>
-                </Row>
+                <div className="card-display">
+                    {projectData.map((item) => (<Thumbnail experience={item} />))}
+                </div>
                 <div id="socialImpact" />
             </section>
             <section className="home-section">
                 <h2 id="socialImpact" className="section-title">Social Impact</h2>
                 <br />
-                <Row>
-                    <Col lg={8}>
-                        <Thumbnail size="small" experience={socialImpactData.nusEnablers} />
-                    </Col>
-                    <Col lg={8}>
-                        <Thumbnail size="small" experience={socialImpactData.projectAgritourism} />
-                    </Col>
-                    <Col lg={8}>
-                        <Thumbnail size="small" experience={socialImpactData.connectTuition} />
-                    </Col>
-                </Row>
+                <div className="card-display">
+                    {socialImpactData.map((item) => (<Thumbnail size="small" experience={item} />))}
+                </div>
             </section>
             <Footer person={personalInfoData} />
         </>
