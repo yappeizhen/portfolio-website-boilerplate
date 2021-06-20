a8af1f52b08846cf7a2ee87f1aa9504317e211bc
yappeizhen <66234273+yappeizhen@users.noreply.github.com>
2021-06-20 14:29:27 +0800

    about page flex box

--- src/views/About.js
+++ src/views/About.js
@@ -8,31 +8,27 @@ function About(props) {
     return (
         <>
             <div style={{ marginTop: '50px' }} className="about-section">
-                <Row>
-                    <Col lg={10}>
-                        <div style={{ display: "inline" }}>
-                            <img className="about-pic" alt="Profile" src={person.img1} />
-                        </div>
-                    </Col>
-                    <Col lg={14}>
-                        <div className="about-me-intro">
-                            <h1>{person.lastName} {person.firstName}</h1>
-                            <p>{person.aboutIntro}</p>
-                        </div>
-                        <div className="about-me-intro">
-                            <h2>Skills</h2>
-                            {skillsList.map((skill) => {
-                                return (<Tag className="skill-tag" key={skill.name} color={skill.color}>{skill.name}</Tag>);
-                            })}
-                        </div>
-                    </Col>
-                </Row>
-                <section style={{textAlign: 'center'}}>
+                <div style={{ display: "inline" }}>
+                    <img className="about-pic" alt="Profile" src={person.img1} />
+                </div>
+                <div className="about-summary">
+                    <div className="about-me-intro">
+                        <h1>{person.lastName} {person.firstName}</h1>
+                        <p>{person.aboutIntro}</p>
+                    </div>
+                    <div className="about-me-intro">
+                        <h2>Skills</h2>
+                        {skillsList.map((skill) => {
+                            return (<Tag className="skill-tag" key={skill.name} color={skill.color}>{skill.name}</Tag>);
+                        })}
+                    </div>
+                </div>
+            </div>
+            <section style={{ textAlign: 'center' }}>
                 <Button href="https://www.linkedin.com/in/peizhen/" size='large' className="big-button" target="_blank" type="primary" shape="circle" icon={<LinkedinOutlined />} />
                 <Button href="https://github.com/yappeizhen" size='large' className="big-button" target="_blank" type="primary" shape="circle" icon={<GithubOutlined />} />
                 <Button href="mailto:yappeizhen@u.nus.edu" size='large' className="big-button" target="_blank" type="primary" shape="circle" icon={<MailOutlined />} />
-                </section>
-            </div>
+            </section>
         </>
     )
 }
