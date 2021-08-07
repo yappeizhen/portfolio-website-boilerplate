42e00564b13335bc5b5ac4789f7b0069ae829a80
yappeizhen <66234273+yappeizhen@users.noreply.github.com>
2021-08-07 20:35:09 +0800

    hide firebase config details

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,9 @@
-import firebase from "firebase/app";
 import 'firebase/storage';
 
+import firebase from "firebase/app";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDLgVY9DalGfvWwTZq_F5j8wowrUiIJ96c",
-    authDomain: "yappeizhen-7ea44.firebaseapp.com",
-    projectId: "yappeizhen-7ea44",
-    storageBucket: "yappeizhen-7ea44.appspot.com",
-    messagingSenderId: "788893744161",
-    appId: "1:788893744161:web:c9645d5b8bdf8e5ef83760",
-    measurementId: "G-CRFPNLD2L4"
-  };
+};
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.storage()
